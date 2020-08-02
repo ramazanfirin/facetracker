db2e74185d6f01cb08116cc09981d4730a7eae1e
DESKTOP-EG60T2A\ramazan <ramazan_firin@hotmail.com>
2020-08-02 19:02:07 +0300

    düzenleme yapıldı

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('facetrackerApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1595919662581)
+        .constant('BUILD_TIMESTAMP', 1596143832286)
 ;
 })();
